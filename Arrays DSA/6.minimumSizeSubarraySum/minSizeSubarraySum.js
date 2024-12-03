a9a02d3df4b1a010c8db563b9c722b3e059603df
edidiesky <essienedidiong1000@gmail.com>
2024-12-03 02:45:44 -0800

    ARRAY BASED QUESTIONS

---
+++ Arrays DSA/6.minimumSizeSubarraySum/minSizeSubarraySum.js
@@ -0,0 +1,18 @@
+function minSizeSubarraySum(nums, target) {
+  let left = 0;
+  let sum = 0;
+  let result = Infinity;
+  for (let end = 0; end < nums.length; end++) {
+    sum += nums[end];
+
+    while (sum >= target) {
+      result = Math.min(result, end - left + 1);
+      sum -= nums[left];
+      left++;
+    }
+  }
+  return result === Infinity ? 0 : result;
+}
+
+let nums = [2, 4, 1, 2, 4, 3];
+console.log(minSizeSubarraySum(nums, 7));
