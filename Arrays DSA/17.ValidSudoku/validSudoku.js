62b91ae40dae2cb3e84e4f81f6be9acf22976f48
edidiesky <essienedidiong1000@gmail.com>
2025-01-20 02:21:04 -0800

    Valid sudoku

---
+++ Arrays DSA/17.ValidSudoku/validSudoku.js
@@ -0,0 +1,4 @@
+function isValidSudoku(board) {
+}
+
+console.log(isValidSudoku());
--- Question.js
+++ Question.js
@@ -99,52 +99,202 @@
 // let numbers = [2, 7, 11, 15, 9];
 
 // console.log(twoSum2(numbers, 16));
+// function threeSum(nums, target) {
+//   let result = [];
 
-function threeSum(nums, target) {
-  let result = [];
+//   for (let i = 0; i < array.length - 2; i++) {
+//     if (i >= 0 && nums[i] === nums[i - 1]) continue;
 
-  for (let i = 0; i < array.length - 2; i++) {
-    if (i >= 0 && nums[i] === nums[i - 1]) continue;
+//     let start = i + 1;
+//     let end = nums.length - 1;
 
-    let start = i + 1;
-    let end = nums.length - 1;
+//     while (start < end) {
+//       let sum = nums[i] + nums[start] + nums[end];
 
-    while (start < end) {
-      let sum = nums[i] + nums[start] + nums[end];
+//       if (sum === target) {
+//         result.push(nums[i], nums[start], nums[end]);
 
-      if (sum === target) {
-        result.push(nums[i], nums[start], nums[end]);
+//         while (nums[start] === nums[start + 1]) {
+//           start++;
+//         }
 
-        while (nums[start] === nums[start + 1]) {
-          start++;
-        }
+//         while (nums[end] === nums[end - 1]) {
+//           end--;
+//         }
 
-        while (nums[end] === nums[end - 1]) {
-          end--;
-        }
+//         start++;
+//         end--;
+//       } else if (sum < target) {
+//         start++;
+//       } else {
+//         end--;
+//       }
+//     }
+//   }
 
-        start++;
-        end--;
-      } else if (sum < target) {
-        start++;
+//   return result;
+// }
+// function getMaxNumber(nums) {
+//   let currentSum = nums[0];
+//   let maxSum = nums[0];
+//   for (let i = 1; i < nums.length; i++) {
+//     currentSum = Math.max(nums[i], nums[i] + currentSum);
+//     maxSum = Math.max(currentSum, maxSum);
+//   }
+//   return maxSum;
+// }
+// console.log(getMaxNumber([-2,1,-3,4,-1,2,1,-5,4]));
+
+// function QuickSort(nums, start = 0, end = nums.length - 1) {
+//   if (start >= end) return nums;
+//   let pivot = getPivot(nums, start, end);
+//   QuickSort(nums, start, pivot - 1);
+//   QuickSort(nums, pivot + 1, end);
+//   return nums;
+// }
+
+// function getPivot(nums, start, end) {
+//   let pivot = nums[start];
+//   let pivotIndex = start;
+//   for (let i = start + 1; i <= end; i++) {
+//     const num = array[i];
+//     if (num < pivot) {
+//       pivotIndex++;
+//       swapIndex(nums, pivotIndex, i);
+//     }
+//   }
+//   swapIndex(nums, start, pivotIndex);
+//   return pivotIndex;
+// }
+
+// function swapIndex(nums, idx1, idx2) {
+//   [nums[idx1], nums[idx2]] = [nums[idx2], nums[idx1]];
+// }
+
+// function minSizeSubArraySum(nums, target) {
+//   let start = 0;
+//   let sum = 0;
+//   let result = Infinity;
+
+//   for (let end = 0; end < nums.length; end++) {
+//     sum += nums[end];
+//     if (sum >= target) {
+//       result = Math.min(result, end - start + 1);
+//       sum -= nums[start];
+//       start++;
+//     }
+//   }
+
+//   return result === Infinity ? 0 : result;
+// }
+// function sortColors(color) {
+//   let start = 0;
+//   let i = 0;
+//   let end = color.length - 1;
+
+//   while (i < end) {
+//     if (color[i] === 0) {
+//       swapColors(color, i, start);
+//       start++;
+//       i++;
+//     } else if (color[i] === 2) {
+//       swapColors(color, i, end);
+//       end--;
+//     } else {
+//       i++;
+//     }
+//   }
+//   return color;
+// }
+
+// function swapColors(nums, idx1, idx2) {
+//   [nums[idx1], nums[idx2]] = [nums[idx2], nums[idx1]];
+// }
+
+// let nums = [2, 1, 0, 1, 2, 0];
+
+// console.log(sortColors(nums));
+
+// function combinationalSum(nums, target) {
+//   let result = [];
+
+//   function backTrack(index, currentSum, currentSubset) {
+//     if (currentSum === target) {
+//       result.push([...currentSubset]);
+//       return;
+//     }
+
+//     if (currentSum > target) {
+//       return;
+//     }
+
+//     for (let i = index; i < nums.length; i++) {
+//       const num = nums[i];
+//       console.log("current_subset_before", currentSubset);
+
+//       currentSubset.push(num);
+//       console.log("current_subset_after", currentSubset);
+//       console.log("current_sum_before", currentSum);
+//       backTrack(i, currentSum + num, currentSubset);
+//       console.log("current_sum_after", currentSum);
+
+//       // console.log("current_backtrack", currentSubset)
+
+//       currentSubset.pop();
+//     }
+//   }
+
+//   backTrack(0, 0, []);
+//   return result;
+// }
+
+// let nums = [2, 5, 6, 9];
+// let target = 9;
+// console.log(combinationalSum(nums, target));
+
+function findMedianOf2SortedArray(nums1, nums2) {
+  if (nums1.length > nums2.length) {
+    [[nums1], [nums2]] = [[nums2], [nums1]];
+  }
+
+  let x = nums1.length;
+  let y = nums2.length;
+
+  let low = 0;
+  let high = x;
+
+  while (low <= high) {
+    let partitionX = Math.floor((low + high) / 2);
+    let partitionY = Math.floor((x + y + 1) / 2) - partitionX; // lefthalf = partitionX + partitionY
+
+    // checking out of bound elements
+    // get maxleft, minRight for both nums1(X) and nums2 (X)
+    let maxLeftX = partitionX === 0 ? Infinity : nums1[partitionX - 1];
+    let minRightX = partitionX === x ? -Infinity : nums1[partitionX];
+
+    let maxLeftY = partitionY === 0 ? Infinity : nums2[partitionY - 1];
+    let minRightY = partitionY === y ? Infinity : nums2[partitionY];
+
+    if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+      if ((x + y) % 2 === 0) {
+        return (
+          (Math.max((maxLeftX, maxLeftY)) + Math.max(minRightX, minRightY)) / 2
+        );
       } else {
-        end--;
+        return Math.max((maxLeftX, maxLeftY));
       }
+    } else if (maxLeftX < minRightY) {
+      low = partitionX + 1;
+    } else {
+      high = partitionX - 1;
     }
   }
+  throw new Error("The input arrays were not sorted");
 
-  return result;
+  //   console.log(([[nums1], [nums2]] = [[nums2], [nums1]]));
 }
 
+let nums1 = [1, 3];
+let nums2 = [2, 4];
 
-function getMaxNumber(nums) {
-  let currentSum = nums[0];
-  let maxSum = nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    currentSum = Math.max(nums[i], nums[i] + currentSum);
-    maxSum = Math.max(currentSum, maxSum);
-  }
-  return maxSum;
-}
-
-console.log(getMaxNumber([-2,1,-3,4,-1,2,1,-5,4]));
+console.log(findMedianOf2SortedArray(nums1, nums2));
